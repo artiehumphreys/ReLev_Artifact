db1b9b1cc3c529040c07c418e6839d33f241e5bc
artiehumphreys <artieh2003@gmail.com>
2026-04-12 11:38:06 -0400

    feat(hls): run script

---
+++ fpga/bloom/run_hls.tcl
@@ -0,0 +1,21 @@
+# Create a project
+open_project -reset proj_bloom
+
+# Add design files
+add_files krnl_bloom.cpp
+
+# Add test bench
+add_files -tb krnl_bloom_test.cpp
+
+# Set the top-level function
+set_top krnl_bloom
+
+# Create a solution
+open_solution -reset solution1
+# Target Alveo U280
+set_part {xcu280-fsvh2892-2L-e}
+create_clock -period 1.25
+
+# Run C-simulation
+csim_design -clean
+exit
